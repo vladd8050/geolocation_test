d8093c51c4d58b292e910f578711a05a6bf85a31
Vlad Shulikov <shulikov.v@gmail.com>
2020-06-01 03:34:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useCallback, useState, useMemo } from 'react';
+
+import { useGeolocation } from 'react-use';
+import { Helmet } from 'react-helmet';
+import { Slider } from 'antd';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+	const [ temp, setTemp ] = useState(0);
+  const [ weatherIco, setWeatherIco ] = useState();
+  const [coord, setCoord] = useState();
+
+  const state = useGeolocation();
+  useEffect(() => {
+    if (state && state.latitude) {
+      const {latitude, longitude} = state
+      setCoord({latitude, longitude})
+    }
+  }, [state])
+  
+	const getWeather = useCallback(
+		async () => {
+			if (coord && coord.latitude) {
+				const { data } = await axios.get(
+					`https://api.openweathermap.org/data/2.5/weather?lat=${coord.latitude}&lon=${coord.longitude}&appid=7f75795554ce4909c726f82e91ab8f7a`
+				);
+				const { weather = [], main: { temp: tTemp = 0 } = {} } = data;
+				if (weather.length) {
+					setWeatherIco(weather[0].icon);
+				}
+
+				setTemp(tTemp - 273.15);
+			}
+		},
+		[ coord ]
+	);
+
+  useEffect(()=>{
+    getWeather();
+  }, [coord, getWeather]);
+
+
+
+	let bodyClassName = useMemo(() => {
+    switch (true) {
+      case temp <= -10:
+        return 'color01';
+      case temp > -10 && temp <= 30:
+        return 'color02';
+      case temp > 30:
+        return 'color03';
+      default:
+        return '';
+    }
+  }, [temp])
+	
+
+	const handleSlider = useCallback((value) => {
+		setTemp(value);
+  }, [setTemp]);
+  
+	return (
+		<div className="App">
+			<Helmet>
+				<body className={bodyClassName} />
+			</Helmet>
+			<header className="App-header">
+				{weatherIco && (
+					<img
+						src={`https://openweathermap.org/img/wn/${weatherIco}@2x.png`}
+						className="App-logo"
+						alt="logo"
+					/>
+				)}
+        <Slider min={-100} value={temp} onChange={handleSlider} style={{ width: 300 }} />
+        <div>Adjust temperature</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
